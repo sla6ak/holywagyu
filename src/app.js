c997039e89d040b94b7db2b4306208fbf44b027f
slabak <slabakxaker@gmail.com>
2023-02-16 19:43:48 +0200

    last

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import { Draggable } from '@shopify/draggable'
+const noRoast = document.getElementById('no-roast')
+const roast = document.getElementById('roast')
+const backdropButton = document.getElementById('backdrop-button')
+const dragableConteiner = document.getElementById('dragable-conteiner')
 
-const draggable = new Draggable(document.querySelectorAll('#dragable'), {
-  draggable: '#backdrop-button'
+console.log(backdropButton.x, backdropButton.y)
+const startPositionX = backdropButton.x
+const startPositionY = backdropButton.y
+
+backdropButton.addEventListener('dragstart', dragstart1)
+backdropButton.addEventListener('dragend', dragend1)
+
+function dragstart1(event) {
+  roast.classList.add('active-res')
+  noRoast.classList.add('active-no-res')
+  roast.style.top = '20px'
+  console.log(roast.style)
+  setTimeout(() => event.target.classList.add('none'), 0)
+}
+function dragend1(event) {
+  roast.classList.remove('active-res')
+  noRoast.classList.remove('active-no-res')
+
+  event.target.classList.remove('none')
+}
+
+dragableConteiner.addEventListener('mousemove', (event) => {
+  console.log('mousemove', event.clientX, event.clientY)
 })
-draggable.on('drag:start', () => console.log('drag:start'))
-draggable.on('drag:move', () => console.log('drag:move'))
-draggable.on('drag:stop', () => console.log('drag:stop'))
